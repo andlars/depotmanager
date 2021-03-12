ae0f1559bd6edff7101ba6c3e13bf67385f6ca48
ShadowWarlord12 <54105795+ShadowWarlord12@users.noreply.github.com>
2021-03-12 09:24:43 +0100

    Created class lager

--- backend/Classes.py
+++ backend/Classes.py
@@ -4,3 +4,9 @@ class bruger:
         self.brugernavn = brugernavn
         self.bruger_ID = bruger_ID
         self.mail = mail
+
+class lager:
+    def __init__(self, dato_for_sidste_optaelling, dato_for_naeste_optaelling, dato):
+        self.dato_for_sidste_optaelling = dato_for_sidste_optaelling
+        self.dato_for_naeste_optaelling = dato_for_naeste_optaelling
+        self.dato = dato
