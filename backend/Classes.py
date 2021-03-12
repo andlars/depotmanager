3fd2fdb69b0d311ee5421a56ce76c93960838d54
stromboli1 <ksg2001@live.com>
2021-03-12 09:27:19 +0100

    Create vare class

--- backend/Classes.py
+++ backend/Classes.py
@@ -4,3 +4,15 @@ class bruger:
         self.brugernavn = brugernavn
         self.bruger_ID = bruger_ID
         self.mail = mail
+
+class vare:
+    def __init__(self, varenummer, varenavn, antal):
+        self.varenummer = varenummer
+        self.varenavn = varenavn
+        self.antal = antal
+
+        def formindskAntal(self,n):
+            self.antal -= n
+
+        def foroegAntal(self,n):
+            self.antal += n
