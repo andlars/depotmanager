23ca583c377d2b3aef33712a81820f92e2082a8d
stromboli1 <ksg2001@live.com>
2021-03-12 08:52:13 +0100

    Tilføjede bruger class

--- backend/Classes.py
+++ backend/Classes.py
@@ -1,4 +1,6 @@
-defkle
-jknjnv
-huhhuec
-jkbjkhi
+class bruger:
+    def __init__(self,admin, brugernavn, bruger_ID, mail):
+        self.admin = admin
+        self.brugernavn = brugernavn
+        self.bruger_ID = bruger_ID
+        self.mail = mail
